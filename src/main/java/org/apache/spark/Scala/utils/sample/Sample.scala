689d7ca83a2d924a6a85ca4921a5b73c69086ab6
chatting <1059257007@qq.com>
2024-05-09 11:13:48 +0800

    update

--- src/main/java/org/apache/spark/Scala/utils/sample/Sample.scala
+++ src/main/java/org/apache/spark/Scala/utils/sample/Sample.scala
@@ -10,7 +10,7 @@ object Sample {
     //    通过计算得出的采样比
     //    val fraction = SamplingUtils.computeFractionForSampleSize(sampleSize, totalNum, false)
     //    println("Sample Fraction: ", fraction)
-    val samples = rdd.takeSample(withReplacement = false, num = 20000, seed = 9961)
+    val samples = rdd.takeSample(withReplacement = false, num = 20000, seed = 9961).map(x => DBScanPoint_3D(x))
     // val samples = rdd.sample(withReplacement = false, sampleRate, seed = 9961).map((x) => DBScanPoint_3D(x))
     samples
   }
